509ca2cb6de9c102c9539d24aa039f56e5b548de
dongguJeong <zzzjdg@naver.com>
2024-10-07 13:30:52 +0900

    기본환경

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <div className="App">
+      <Reset />
+      <div>Hi, I'm an app!</div>
+    </div>
+  );
+}
+
+export default App;
