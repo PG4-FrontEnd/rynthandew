57ea12083913e290dde12bfd98f08720e0c2ddf2
dongguJeong <zzzjdg@naver.com>
2024-10-09 14:07:45 +0900

    feat : CustomInput 생성
    공통 컴포넌트 input 박스 생성

---
+++ src/utils/types.d.ts
@@ -0,0 +1,12 @@
+declare interface InputProps {
+  width: number;
+  type: string;
+  placeholder: string;
+  error?: string;
+  label?: string;
+  height?: number;
+  background?: string;
+  required?: string;
+  fontSize?: number;
+  iconLeft?: string;
+}
