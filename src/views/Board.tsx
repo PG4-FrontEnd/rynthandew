eadfabe999e1b13cc339b95c172a8284f78b19e0
dongguJeong <zzzjdg@naver.com>
2024-10-10 23:41:16 +0900

    fix : "Board.tsx에서 pageheader 삭제"
    Board.tsx에서 pageheader 삭제

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import PageHeader from '../components/PageHeader.tsx';
 
 export default function Board() {
-  return (
-    <div>
-      <PageHeader title="제목" />
-      Board
-    </div>
-  );
+  return <div>Board</div>;
 }
