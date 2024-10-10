b3158919b6c85e8f9168ce3f2976944ab05f6bb2
dongguJeong <zzzjdg@naver.com>
2024-10-10 23:33:36 +0900

    feat: "페이지 헤더  구현"
    페이지 헤더 구현
    Breadcrumbs 구현
    
    타입 설정할 때 디폴트 밸류 설정하지 않아도 되게끔 eslintrc 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { Reset } from 'styled-reset';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home.tsx';
+import Board from './views/Board.tsx';
+import Bulletin from './views/Bulletin.tsx';
+import Join from './views/Join.tsx';
+import Login from './views/Login.tsx';
+import Notification from './views/Notification.tsx';
+import Setting from './views/Setting.tsx';
+import Writing from './views/Writing.tsx';
 
 function App() {
   return (
     <div className="App">
       <Reset />
-      <div>Hi, I am app</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/join" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/board/setting" element={<Setting />} />
+          <Route path="/bulletin" element={<Bulletin />} />
+          <Route path="/bulletin/Writing" element={<Writing />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageHeader from '../components/PageHeader.tsx';
+
+export default function Board() {
+  return (
+    <div>
+      <PageHeader title="제목" />
+      Board
+    </div>
+  );
+}
