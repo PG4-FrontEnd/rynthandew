9e05a0ef53b55c24950ec0f274d273b438945500
dongguJeong <zzzjdg@naver.com>
2024-10-11 14:00:44 +0900

    feat: "카드 css 수정. 닫기 버튼 추가, 타입 수정"
    카드 css 수정
    닫기 버튼 추가
    onClick event 받을 수 있도록 타입 수정

--- src/components/BoardCard.tsx
+++ src/components/BoardCard.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 import AccountCircleFilledSvg from '../utils/AccountCircleFilledSvg.tsx';
+import { CloseSVG } from '../utils/CloseSvg.tsx';
 
 interface BoardCardProp {
   title: string;
-  manager: string;
+  manager?: string;
+  height?: number;
+  width?: number;
+  fontSize?: number;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  closeClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-const BoardCardContainer = styled.div`
+const CloseSVGdiv = styled.div<{ size: number }>`
+  opacity: 0;
+  visibility: hidden;
+  transition:
+    opacity 0.1s ease-in,
+    visibility 0.1s ease-in;
+  width: ${props => `${props.size}px`};
+  height: ${props => `${props.size}px`};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const BoardCardContainer = styled.div<{ width: number; height: number }>`
   padding: 10px;
-  width: 252px;
-  height: 70px;
-  border: 1px solid black;
+  box-sizing: border-box;
+  width: ${props => `${props.width}px`};
+  height: ${props => `${props.height}px`};
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   background-color: white;
+
   &:hover {
     background-color: #e9ecee;
+
+    ${CloseSVGdiv} {
+      opacity: 1;
+      visibility: visible;
+    }
   }
 
   &:focus {
@@ -26,37 +53,79 @@ const BoardCardContainer = styled.div`
 const BoardCardContainerFlexCol = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  width: 100%;
   justify-content: space-between;
 `;
 
-const BoardCardContainerTitle = styled.span`
-  font-size: 16px;
+const BoardCardContainerTitleDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BoardCardContainerTitle = styled.span<{ fontSize: number }>`
+  font-size: ${props => props.fontSize}px;
   font-weight: 500;
+`;
+
+const ManagerIndicator = styled.div`
+  opacity: 0;
+  visibility: hidden;
+  position: absolute;
+  border-radius: 4px;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  padding: 4px;
+  width: 100px;
+  height: 16px;
+  font-size: 12px;
+  color: black;
+  bottom: -30px;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.6);
+  transition:
+    opacity 0.1s ease-in,
+    visibility 0.1s ease-in;
 `;
 
 const AccountCircleFilledSvgDiv = styled.div`
   width: fit-content;
   position: relative;
+
+  &:hover {
+    ${ManagerIndicator} {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+
+  transition: all 0.1s ease-in;
 `;
 
-const ManagerIndicator = styled.div`
-  position: absolute;
-  background-color: white;
-  width: 50px;
-  height: 20px;
-  font-size: 14px;
-  color: black;
-  bottom: -40px;
-`;
-
-export default function BoardCard({ title, manager }: BoardCardProp) {
+export default function BoardCard({
+  title,
+  manager = '없음',
+  height = 80,
+  width = 220,
+  onClick = () => {},
+  closeClick = () => {},
+  fontSize = 16,
+}: BoardCardProp) {
   return (
-    <BoardCardContainer>
+    <BoardCardContainer width={width} height={height} onClick={onClick}>
       <BoardCardContainerFlexCol>
-        <BoardCardContainerTitle>{title}</BoardCardContainerTitle>
+        <BoardCardContainerTitleDiv>
+          <BoardCardContainerTitle fontSize={fontSize}>
+            {title.length < 10 ? title : `${title.slice(0, 10)}...`}
+          </BoardCardContainerTitle>
+          <CloseSVGdiv onClick={closeClick} size={fontSize}>
+            <CloseSVG width={fontSize} height={fontSize} />
+          </CloseSVGdiv>
+        </BoardCardContainerTitleDiv>
         <AccountCircleFilledSvgDiv>
           <AccountCircleFilledSvg />
-          <ManagerIndicator>{manager}</ManagerIndicator>
+          <ManagerIndicator>담당자 : {manager}</ManagerIndicator>
         </AccountCircleFilledSvgDiv>
       </BoardCardContainerFlexCol>
     </BoardCardContainer>
---
+++ src/utils/CloseSvg.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface svgProp {
+  width?: number;
+  height?: number;
+}
+
+export const CloseSVG = ({ width = 16, height = 16 }: svgProp) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 19.1667C15.0627 19.1667 19.1667 15.0626 19.1667 10C19.1667 4.9374 15.0627 0.833344 10 0.833344C4.93743 0.833344 0.833374 4.9374 0.833374 10C0.833374 15.0626 4.93743 19.1667 10 19.1667Z"
+        fill="#ACB4BB"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.70334 9.99958L4.76819 13.9347L6.06455 15.2311L9.9997 11.2959L13.9349 15.2311L15.2312 13.9347L11.2961 9.99958L15.2312 6.06443L13.9349 4.76807L9.9997 8.70322L6.06455 4.76807L4.76819 6.06443L8.70334 9.99958Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
