db4934cff566e13da0f1ae689673ab5d0b76f982
dongguJeong <zzzjdg@naver.com>
2024-10-12 13:48:00 +0900

    fix : "커스텀 인풋의 함수 전달 기능 삭제 및 svgsprite 적용"
    외부에서 커스텀 인풋의 value를 조작할 수 있게 만들었으나, 커스텀 인풋을 사용할 때 useState를 적용하지 않으면 아예 입력이 불가능해지고 커스텀 인풋을 여러개 사용해야 하는 경우 그만큼 useState를 많이 사용하게 만들어야 하는 것이 비효율적이라고 판단했습니다.
    
    svgsprite를 적용해 기존 svg를 교체하였습니다. 커스텀 인풋 박스를 바꾸는 김에 boardCard도 svg이미지를 svgsprite를 적용하는 것으로 바꾸었습니다.

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import CustomInput from '../components/CustomInput.tsx';
+import BoardCard from '../components/BoardCard.tsx';
 
 export default function Board() {
-  return <div>Board</div>;
+  return (
+    <div>
+      <CustomInput width={220} type="text" placeholder="아이디" />
+      <BoardCard title="풀스택 개발자로 거듭나기" />
+      Board
+    </div>
+  );
 }
