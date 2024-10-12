bb5fd329e3b6ec53e4aea563b1535db66bf52f45
dongguJeong <133619736+dongguJeong@users.noreply.github.com>
2024-10-12 15:01:23 +0900

    Update Board.tsx

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -5,8 +5,6 @@ import BoardCard from '../components/BoardCard.tsx';
 export default function Board() {
   return (
     <div>
-      <CustomInput width={220} type="text" placeholder="아이디" />
-      <BoardCard title="풀스택 개발자로 거듭나기" />
       Board
     </div>
   );
