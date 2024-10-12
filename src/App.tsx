2a5b3a9adc118cbb6c5c926c1e380d438706ddcd
dongguJeong <133619736+dongguJeong@users.noreply.github.com>
2024-10-12 15:52:32 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Reset } from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home.tsx';
@@ -9,11 +10,24 @@ import Login from './views/Login.tsx';
 import Notification from './views/Notification.tsx';
 import Setting from './views/Setting.tsx';
 import Writing from './views/Writing.tsx';
+import { svgSpriteSheet } from './utils/SvgSprite.tsx';
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    --selected : #EDF4Fb ;
+    --main : #0086CB;
+  }
+  body{
+    font-family : 'Roboto', sans-serif;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
       <Reset />
+      <GlobalStyle />
+      {svgSpriteSheet}
       <BrowserRouter>
         <Routes>
           <Route path="/join" element={<Join />} />
