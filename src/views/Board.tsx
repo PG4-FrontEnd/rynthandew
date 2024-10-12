dee1469466dae958c943e652ff728f46b78d9cc2
dongguJeong <133619736+dongguJeong@users.noreply.github.com>
2024-10-12 19:44:56 +0900

    Update Board.tsx

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import CustomInput from '../components/CustomInput.tsx';
-import BoardCard from '../components/BoardCard.tsx';
 
 export default function Board() {
   return (
