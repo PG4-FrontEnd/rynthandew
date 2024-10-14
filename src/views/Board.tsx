3a4a0ffdd31337926adb9461a11ee8bcc0f0214e
dongguJeong <zzzjdg@naver.com>
2024-10-14 12:49:19 +0900

    fix : "PageHeaderContainer를 PageHeaderContainerForm으로 변경"
    onSubmit 이벤트를 받을 수 있도록 div였던 PageHeaderContainer를 form인 PageHeaderContainerForm으로 변경

--- src/views/Board.tsx
+++ src/views/Board.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import CustomInput from '../components/CustomInput.tsx';
-import BoardCard from '../components/BoardCard.tsx';
 
 export default function Board() {
-  return (
-    <div>
-      Board
-    </div>
-  );
+  return <div>Board</div>;
 }
